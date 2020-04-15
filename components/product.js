6202ccab870897f0939c84d6c8db75b84d556d91
Łukasz Hołdrowicz <lukasz.holdrowicz@gmail.com>
2020-04-15 21:00:44 +0200

    Ugly styles.

--- components/product.js
+++ components/product.js
@@ -23,62 +23,77 @@ Vue.component('product', {
         }
     },
     template: `
-        <div class="product">
-            <div class="product-image">
-                <img :src="image" />
-            </div>
-            <div class="product-info">
-                <!-- Simple binding that can be later changed by app.product = something -->
-                <h1>{{title}}</h1>
-                
-                <!-- IF LSE-->
-                <!-- i can use array for more than one style/class -->
-                <div class="description" :style="[quantity <= almostSoldOutQuantity ? {'color': 'red' } : {}]">
-                    <p v-if="quantity > almostSoldOutQuantity">In Stock ({{quantity}})</p>
-                    <p v-else-if="quantity > 0">Almost sold out, there are only {{quantity}} left</p>
-                    <p v-else>Out of Stock</p>
-                    <p>Shipping: {{shipping}}</p>
+        <div class="product container">
+            <!-- Simple binding that can be later changed by app.product = something -->
+            <div class="row">
+                <div class="col-6">
+                    <div class="row both-center">
+                        <div class="product-image">
+                            <img :src="image" class="image-responsive" />
+                        </div>
+                    </div>
+                    <div class="row both-center">
+                        <div 
+                            v-for="(variant, index) in productData.variants" 
+                            :key="variant.id"
+                            class="color-box"
+                            :style="{backgroundColor: variant.color, 'font-size': '1.1em'}"
+                            @mouseover="colorHover(index)"
+                        ></div>
+                    </div>
                 </div>
+                <div class="product-info col-6">  
+                    <h1 class="title">{{title}}</h1>  
+                    <div class="">
+                        <!-- IF LSE-->
+                        <!-- i can use array for more than one style/class -->
+                        <div class="product-description" :style="[quantity <= almostSoldOutQuantity ? {'color': 'red' } : {}]">
+                            <p v-if="quantity > almostSoldOutQuantity">In Stock ({{quantity}})</p>
+                            <p v-else-if="quantity > 0">Almost sold out, there are only {{quantity}} left</p>
+                            <p v-else>Out of Stock</p>
+                            <p>Shipping: {{shipping}}</p>
+                        </div>
 
-                <!-- Simple for-->
-                <div v-show="detailsVisible">
-                    <ul>
-                        <li v-for="detail in productData.details">
-                            {{detail}}
-                        </li>
-                    </ul>
+                        <div class="product-price">
+                            {{variantPrice}} $
+                        </div>
+
+                        <!-- Simple for-->
+                        <div v-show="detailsVisible" class="product-details">
+                            <ul>
+                                <li v-for="detail in productData.details">
+                                    {{detail}}
+                                </li>
+                            </ul>
+                        </div>
+                        
+                        <hr />
+
+                        <!-- Disabling button based on config data object -->
+                        <div class="product-buttons">
+                            <button 
+                                v-on:click="addToCart" 
+                                :disabled="!inStock"
+                                :class="{ disabledButton: !inStock }"
+                                class="button button-success"
+                            >
+                                Add to cart
+                            </button>
+                            <button 
+                                v-on:click="removeFromCart" 
+                                :disabled="!canRemove"
+                                :class="[{ disabledButton: !canRemove }, { 'button-danger': canRemove }]"
+                                class="button"
+                            >
+                                Remove
+                            </button>
+                        </div>
+                        
+                        <!--
+                            <product-comment-form @comment-added="commentAdded"></product-comment-form>
+                        -->
+                    </div>
                 </div>
-
-                <h4>Colors</h4>
-
-                <!-- Style binding, for, key, event handler binding, using method from new Vue({config})-->
-                <div 
-                    v-for="(variant, index) in productData.variants" 
-                    :key="variant.id"
-                    class="color-box"
-                    :style="{backgroundColor: variant.color, 'font-size': '1.1em'}"
-                    @mouseover="colorHover(index)"
-                >
-                    
-                </div>
-
-                <!-- Disabling button based on config data object -->
-                <button 
-                    v-on:click="addToCart" 
-                    :disabled="!inStock"
-                    :class="{ disabledButton: !inStock }"
-                >
-                    Add to cart
-                </button>
-                <button 
-                    v-on:click="removeFromCart" 
-                    :disabled="!canRemove"
-                    :class="[{ disabledButton: !canRemove }, { 'remove-button': canRemove }]"
-                >
-                    Remove
-                </button>
-                <br />
-                <product-comment-form @comment-added="commentAdded"></product-comment-form>
             </div>
         </div>
     `,
@@ -128,6 +143,9 @@ Vue.component('product', {
             const originalQuantity = this.productDataOriginal.variants[this.selectedVariant].quantity;
             const currentQuantity = this.productData.variants[this.selectedVariant].quantity;
             return originalQuantity > currentQuantity;
+        },
+        variantPrice() {
+            return this.productData.variants[this.selectedVariant].price;
         },
         shipping() {
             if(this.premium) {
